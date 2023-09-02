5bddd11881a7eef29793d8298e902d62d7902c47
isturiz <isturizmauricio@gmail.com>
2023-09-02 07:53:14 -0400

    update gitsigns options

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -1,3 +1,10 @@
 return {
   "lewis6991/gitsigns.nvim",
+  event = "VeryLazy",
+  opts = {
+    signcolumn = false,
+    numhl = true,
+    -- trouble = false,
+    max_file_length = 10000,
+  },
 }
