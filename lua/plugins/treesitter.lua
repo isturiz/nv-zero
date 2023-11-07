59b7e03b6a05aac5eec02e73c462ce809f5999b8
isturiz <isturizmauricio@gmail.com>
2023-11-07 17:05:58 -0400

    lua and treesitter config

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,37 +1,54 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  enabled = true,
-  build = ":TSUpdate",
-  config = function()
-    require("nvim-treesitter.configs").setup {
-      ensure_installed = {
-        "css",
-        "html",
-        "javascript",
-        "json",
-        "lua",
-        "luadoc", -- investigar diferencia con lua
-        "markdown",
-        "markdown_inline", -- investigar
-        -- "typescript",
-        "regex",
-        "vim",
-      },
-      highlight = {
-        enable = true,
-      },
-      indent = {
-        enable = true,
-      },
-      incremental_selection = {
-        enable = true,
-        keymaps = {
-          init_selection = "gnn",
-          node_incremental = "grn",
-          scope_incremental = "grc",
-          node_decremental = "grm",
-        },
-      },
-    }
-  end,
+    "nvim-treesitter/nvim-treesitter",
+    enabled = true,
+    build = ":TSUpdate",
+    config = function()
+        require("nvim-treesitter.configs").setup {
+            ensure_installed = {
+
+                -- Web
+                "html",
+                "css",
+                "javascript",
+
+                -- Others Web
+                "json",
+                "typescript",
+
+                -- Lua
+                "lua",
+                "luadoc",  -- investigar diferencia con lua
+
+                -- Vim
+                "vim",
+
+                -- Markdown
+                "markdown",
+                "markdown_inline", -- investigar
+
+                -- Regular Expression
+                "regex",
+                
+                -- bash
+                "bash"
+            },
+            ensure_installed = "maintained", -- Instala los parsers para los lenguajes mantenidos
+
+            highlight = {
+                enable = true,
+            },
+            indent = {
+                enable = true,
+            },
+            incremental_selection = {
+                enable = true,
+                keymaps = {
+                    init_selection = "gnn",
+                    node_incremental = "grn",
+                    scope_incremental = "grc",
+                    node_decremental = "grm",
+                },
+            },
+        }
+    end,
 }
