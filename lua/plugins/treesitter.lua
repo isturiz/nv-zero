c6a1377a9f9caba40ebc2158b09688d26c5bfebb
isturiz <isturizmauricio@gmail.com>
2023-11-07 17:23:42 -0400

    add autotag to treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,54 +1,66 @@
 return {
-    "nvim-treesitter/nvim-treesitter",
-    enabled = true,
-    build = ":TSUpdate",
-    config = function()
-        require("nvim-treesitter.configs").setup {
-            ensure_installed = {
+    {
+        "nvim-treesitter/nvim-treesitter",
+        enabled = true,
+        build = ":TSUpdate",
+        config = function()
+            require("nvim-treesitter.configs").setup {
+                ensure_installed = {
 
-                -- Web
-                "html",
-                "css",
-                "javascript",
+                    -- Web
+                    "html",
+                    "css",
+                    "javascript",
 
-                -- Others Web
-                "json",
-                "typescript",
+                    -- Others Web
+                    "json",
+                    "typescript",
 
-                -- Lua
-                "lua",
-                "luadoc",  -- investigar diferencia con lua
+                    -- Lua
+                    "lua",
+                    "luadoc", -- investigar diferencia con lua
 
-                -- Vim
-                "vim",
+                    -- Vim
+                    "vim",
 
-                -- Markdown
-                "markdown",
-                "markdown_inline", -- investigar
+                    -- Markdown
+                    "markdown",
+                    "markdown_inline", -- investigar
 
-                -- Regular Expression
-                "regex",
-                
-                -- bash
-                "bash"
-            },
-            ensure_installed = "maintained", -- Instala los parsers para los lenguajes mantenidos
+                    -- Regular Expression
+                    "regex",
 
-            highlight = {
-                enable = true,
-            },
-            indent = {
-                enable = true,
-            },
-            incremental_selection = {
-                enable = true,
-                keymaps = {
-                    init_selection = "gnn",
-                    node_incremental = "grn",
-                    scope_incremental = "grc",
-                    node_decremental = "grm",
+                    -- bash
+                    "bash"
+                },
+
+                highlight = {
+                    enable = true,
+                },
+                indent = {
+                    enable = true,
+                },
+                incremental_selection = {
+                    enable = true,
+                    keymaps = {
+                        init_selection = "gnn",
+                        node_incremental = "grn",
+                        scope_incremental = "grc",
+                        node_decremental = "grm",
+                    },
+                },
+            }
+        end,
+    },
+    {
+        "windwp/nvim-ts-autotag",
+        dependencies = {
+            "nvim-treesitter/nvim-treesitter",
+            opts = {
+                autotag = {
+                    enable = true,
                 },
             },
-        }
-    end,
+        },
+    }
 }
